03760fc0f274b746e8a6b3ee442d5fd53b07a446
Fransiskus <aldzaidea@gmail.com>
2022-02-07 20:23:37 +0000

    add 1st queries

---
+++ 1_queries/1_queries.sql
@@ -0,0 +1,41 @@
+-- select their id and name
+-- Order them by their name in alphabetical order.
+-- Since this query needs to work with any specific cohort, 
+-- just use any number for the cohort_id.
+
+SELECT id, name 
+FROM students 
+WHERE cohort_id = 1
+ORDER BY name;
+
+-- Select the total number of students who were in the first 3 cohorts.
+
+SELECT count(id)
+FROM students 
+WHERE cohort_id IN (1,2,3);
+
+-- Get all of the students that don't have an email or phone number.
+SELECT name, id, cohort_id
+FROM students
+WHERE email IS NULL
+OR phone IS NULL;
+
+-- Get all of the students without a gmail.com account and a phone number.
+SELECT name, id, email, cohort_id
+FROM students
+WHERE email NOT LIKE '%gmail.com'
+AND phone IS NULL;
+
+-- Get all of the students currently enrolled.
+
+SELECT name, id, cohort_id
+FROM students
+WHERE end_date IS NULL
+ORDER BY cohort_id;
+
+-- Get all graduates without a linked Github account.
+
+SELECT name, email, phone
+FROM students
+WHERE github IS NULL
+AND end_date IS NOT NULL;
