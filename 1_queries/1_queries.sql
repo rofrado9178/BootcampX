b104fdac8563de34e49a710d34ce0334f86e1f9c
Fransiskus <aldzaidea@gmail.com>
2022-02-07 21:38:43 +0000

    add JOIN queries

--- 1_queries/1_queries.sql
+++ 1_queries/1_queries.sql
@@ -39,3 +39,36 @@ SELECT name, email, phone
 FROM students
 WHERE github IS NULL
 AND end_date IS NOT NULL;
+
+----------------------------- JOIN TABLE ----------------------------------
+SELECT students.name as student_name, email, cohorts.name as cohort_name
+FROM students JOIN cohorts ON cohorts.id = cohort_id;
+
+-- INNER JOIN , notes -> data without cohort_id or foreign key is null will not appear
+SELECT students.name as student_name, email, cohorts.name as cohort_name
+FROM students INNER JOIN cohorts ON cohorts.id = cohort_id;
+
+--------------------------- OUTER JOIN ------------------------------ 
+--we need to specify either LEFT, RIGHT or FULL
+
+
+
+--LEFT OUTER JOIN -> will show student even without cohort_id
+SELECT students.name as student_name, email, cohorts.name as cohort_name
+FROM students LEFT OUTER JOIN cohorts ON cohorts.id = cohort_id;
+--RIGHT OUTER JOIN -> will show even without any students enrolled
+SELECT students.name as student_name, email, cohorts.name as cohort_name
+FROM students RIGHT OUTER JOIN cohorts ON cohorts.id = cohort_id;
+--FULL OUTER JOIN -> will return cohorts and all students even when the is no match
+SELECT students.name as student_name, email, cohorts.name as cohort_name
+FROM students FULL OUTER JOIN cohorts ON cohorts.id = cohort_id;
+
+
+-- We could also rewrite any RIGHT JOIN as a LEFT JOIN, 
+-- just by changing the order of the tables.
+-- So the following two queries would produce identical results:
+
+FROM students LEFT JOIN cohorts ON cohorts.id = cohort_id;
+FROM cohorts RIGHT JOIN students ON cohorts.id = cohort_id;
+-- THE RESULT WILL BE THE SAME
+
